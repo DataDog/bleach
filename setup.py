ee7d2a50f31853d38b617751a30820066ff10a1c
Dave Coleman <your_email@datadoghq.com>
2016-02-29 21:03:47 +0000

    added commits from https://github.com/mozilla/bleach/pull/149 . we can remove this fork when new version of bleach containing these commits is released

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except ImportError:
 
 setup(
     name='bleach',
-    version='1.4.2',
+    version='1.4.2+dd.1',
     description='An easy whitelist-based HTML-sanitizing tool.',
     long_description=open('README.rst').read(),
     author='James Socol',
